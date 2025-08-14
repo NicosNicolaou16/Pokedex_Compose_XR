40abded5240a05e3d8f88216b474fd2be973aafa
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-14 22:34:48 +0300

    improve the background thread

--- app/src/main/java/com/nicos/pokedex_compose_xr/domain/repositories/PokemonDetailsRepository.kt
+++ app/src/main/java/com/nicos/pokedex_compose_xr/domain/repositories/PokemonDetailsRepository.kt
@@ -2,9 +2,10 @@ package com.nicos.pokedex_compose_xr.domain.repositories
 
 import com.nicos.pokedex_compose_xr.utils.generic_classes.Resource
 import com.nicos.pokedex_compose_xr.data.room_database.entities.PokemonDetailsEntity
+import kotlinx.coroutines.flow.Flow
 
 interface PokemonDetailsRepository {
-    suspend fun fetchPokemonDetails(url: String, name: String): Resource<PokemonDetailsEntity>
+    suspend fun fetchPokemonDetails(url: String, name: String): Flow<Resource<PokemonDetailsEntity>>
     suspend fun savePokemonDetails(pokemonDetailsEntity: PokemonDetailsEntity)
-    suspend fun offline(name: String): Resource<PokemonDetailsEntity>
+    suspend fun offline(name: String): Flow<Resource<PokemonDetailsEntity>>
 }
