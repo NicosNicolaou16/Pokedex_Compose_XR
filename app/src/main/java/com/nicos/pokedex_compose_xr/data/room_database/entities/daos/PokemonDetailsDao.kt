83a96f6b28c1f65aca4cfd46f76c8bcaae7a1da2
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-16 20:31:23 +0300

    create the data model for the one to many entities

--- app/src/main/java/com/nicos/pokedex_compose_xr/data/room_database/entities/daos/PokemonDetailsDao.kt
+++ app/src/main/java/com/nicos/pokedex_compose_xr/data/room_database/entities/daos/PokemonDetailsDao.kt
@@ -2,12 +2,24 @@ package com.nicos.pokedex_compose_xr.data.room_database.entities.daos
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import com.nicos.pokedex_compose_xr.data.room_database.entities.PokemonDetailsEntity
+import com.nicos.pokedex_compose_xr.data.room_database.entities.PokemonDetailsWithStatsEntity
 import com.nicos.pokedex_compose_xr.data.room_database.init_database.BaseDao
 
 @Dao
 interface PokemonDetailsDao : BaseDao<PokemonDetailsEntity, MutableList<PokemonDetailsEntity>> {
 
+    @Transaction
     @Query("SELECT * FROM pokemondetailsentity WHERE name=:name")
     suspend fun getPokemonInfoByName(name: String): PokemonDetailsEntity?
+
+    @Transaction
+    @Query("SELECT * FROM pokemondetailsentity WHERE name=:name")
+    suspend fun getPokemonDetailsWithStatsByName(name: String): PokemonDetailsWithStatsEntity?
+
+    @Transaction
+    @Query("DELETE FROM pokemondetailsentity WHERE name=:name")
+    suspend fun deletePokemonInfoByName(name: String)
+
 }
