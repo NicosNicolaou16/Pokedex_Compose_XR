daa3d048ff6c76b923c33eaacf304579d6cd5093
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-16 20:56:45 +0300

    minor fixes

--- app/src/main/java/com/nicos/pokedex_compose_xr/presentation/pokemon_list_screen/PokemonListScreen.kt
+++ app/src/main/java/com/nicos/pokedex_compose_xr/presentation/pokemon_list_screen/PokemonListScreen.kt
@@ -4,16 +4,11 @@ package com.nicos.pokedex_compose_xr.presentation.pokemon_list_screen
 
 import android.annotation.SuppressLint
 import androidx.activity.SystemBarStyle
-import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -36,7 +31,6 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import androidx.xr.compose.spatial.Subspace
 import androidx.xr.compose.subspace.SpatialPanel
 import androidx.xr.compose.subspace.SpatialRow
