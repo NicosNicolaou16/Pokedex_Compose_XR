0b2d04116ad68b538ac2ddbf7bd7cd3c5e8e8efa
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-09-06 00:28:34 +0300

    example with dto

--- app/src/main/java/com/nicos/pokedex_compose_xr/domain/models/pokemon_response_model/PokemonResponse.kt
+++ app/src/main/java/com/nicos/pokedex_compose_xr/domain/models/pokemon_response_model/PokemonResponse.kt
@@ -1,7 +1,6 @@
 package com.nicos.pokedex_compose_xr.domain.models.pokemon_response_model
 
 import com.google.gson.annotations.SerializedName
-import com.nicos.pokedex_compose_xr.data.room_database.entities.PokemonEntity
 import com.nicos.pokedex_compose_xr.domain.dto.PokemonDto
 
 data class PokemonResponse(
