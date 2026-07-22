628a31b9823508e46eb461db73bcd478d8996da6
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-22 21:55:00 +0300

    migrate to Room 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.parcelize)
+    alias(libs.plugins.androidx.room)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.parcelize) apply false
+    alias(libs.plugins.androidx.room) apply false
 }
