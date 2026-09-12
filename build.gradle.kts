fb75540f3ea1cbd4a1222d3e9313e7f057d1f15f
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-09-13 00:34:28 +0300

    setup for baseline profile

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.parcelize) apply false
     alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.android.test) apply false
 }
